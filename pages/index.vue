25cf3c8fd9861c08781fe90a28675c7eed1a3697
Elijah Kelly <ekelly14@ymail.com>
2019-06-19 19:10:32 -0500

    Add Navbar to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,28 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        personal-website
-      </h1>
-      <h2 class="subtitle">
-        A personal Website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+  <section>
+    <NavBar/>
+    <div class="container">
+      <div>
+        <logo/>
+        <h1 class="title">personal-website</h1>
+        <h2 class="subtitle">A personal Website</h2>
+        <div class="links">
+          <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import NavBar from '~/components/NavBar.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    NavBar
   }
 }
 </script>

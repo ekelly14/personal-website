7b8cb414db492d29d341e2b1774e6682d8d80fa9
Elijah Kelly <ekelly14@ymail.com>
2019-06-20 20:38:34 -0500

    Added logic for changing nav bar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,25 +1,54 @@
 <template>
-  <section class="flex flex-row bg-teal-300 mx-auto">
-    <nuxt-link to="/">
-      <div class="px-10 py-5 w-auto hover:bg-teal-400 hover:text-white">Home</div>
-    </nuxt-link>
-    <nuxt-link to="/about">
-      <div class="px-10 py-5 w-auto hover:bg-teal-400 hover:text-white">About Me</div>
-    </nuxt-link>
-    <nuxt-link to="/about">
-      <div class="px-10 py-5 w-auto hover:bg-teal-400 hover:text-white">Hobbies</div>
-    </nuxt-link>
-    <a href="https://github.com/ekelly14">
-      <div class="px-10 py-5 hover:bg-teal-400 hover:text-white">GitHub</div>
-    </a>
-  </section>
+  <no-ssr>
+    <section
+      class="flex flex-row mx-auto fixed bg-teal-300 w-full"
+      v-bind:class="{ scrolled: isScrolled }"
+    >
+      <nuxt-link to="/">
+        <div class="px-10 py-5 w-auto hover:bg-teal-400 hover:text-white">Home</div>
+      </nuxt-link>
+      <nuxt-link to="/about">
+        <div class="px-10 py-5 w-auto hover:bg-teal-400 hover:text-white">About Me</div>
+      </nuxt-link>
+      <nuxt-link to="/about">
+        <div class="px-10 py-5 w-auto hover:bg-teal-400 hover:text-white">Hobbies</div>
+      </nuxt-link>
+      <a href="https://github.com/ekelly14">
+        <div class="px-10 py-5 hover:bg-teal-400 hover:text-white">GitHub</div>
+      </a>
+    </section>
+  </no-ssr>
 </template>
 
 <script>
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  data() {
+    return {
+      isScrolled: false
+    }
+  },
+  beforeMount() {
+    //Check if the page is already scrolled
+    this.handleScroll()
+    //initializes the event listener for scroll
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy() {
+    //cleanup events before destroy
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      //Check if the page is scrolled from the original position
+      this.isScrolled = window.pageYOffset > 35
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.scrolled {
+  background: white;
+}
 </style>

a8fca82a0145253b5f2f0d33474d461f5ed55aff
Elijah Kelly <ekelly14@ymail.com>
2019-08-10 23:57:44 -0500

    Update styling and layout

--- components/general/TextBlock.vue
+++ components/general/TextBlock.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="mx-auto block-style mt-10" style="max-width: 1200px;" :class="{ 'bottom-border' : borderBottom }">
-    <div v-if="title" class="w-full text-4xl">
+    <div v-if="title" class="w-full text-4xl pb-4">
       {{ title }}
     </div>
     <div v-if="!textOnRight" class="flex flex-row w-full h-full mb-10">
       <div class="h-full text-block pr-5 text-xl">
         <slot />
       </div>
-      <div class="picture-block" :style="`background-image: url(/images/${imageUrl});`" />
+      <div class="picture-block" :style="`background-image: url(/images/${imageUrl});`" :class="{ 'picture-circle' : roundImage }" />
     </div>
 
     <div v-if="textOnRight" class="flex flex-row w-full h-full mb-10">
-      <div class="picture-block" :style="`background-image: url(/images/${imageUrl});`" />
+      <div class="picture-block" :style="`background-image: url(/images/${imageUrl});`" :class="{ 'picture-circle' : roundImage }" />
       <div class="h-full text-block pl-5 text-xl">
         <slot />
       </div>
@@ -38,6 +38,10 @@
       imageUrl: {
         type: String,
         default: ''
+      },
+      roundImage: {
+        type: Boolean,
+        default: false
       }
     }
   }
@@ -49,7 +53,6 @@
 }
 .text-block {
   flex-basis: 55%;
-  height: 500px;
 }
 .picture-block {
   background-color: aquamarine;
@@ -58,5 +61,11 @@
   background-repeat: no-repeat;
   flex-basis: 45%;
   height: 50vh;
+}
+.picture-circle {
+  max-width: 50vh;
+  border-radius: 50%;
+  right: 0;
+  margin-left: auto;
 }
 </style>

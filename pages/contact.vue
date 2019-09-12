82455cc7d724ec4fe22209b28da167b6109439a7
Elijah Kelly <ekelly14@ymail.com>
2019-09-11 23:17:27 -0500

    Start implementation on contact links

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,11 +3,25 @@
     <TitleBar />
     <div class="container mx-auto">
       <div class="home">
-        Contact Me Page still under construction! 
-        <br />
-        <nuxt-link to="/" class="text-blue-dark">
-          Back
-        </nuxt-link>
+        <a href="https://github.com/ekelly14">
+          <div class="link-container">
+            <strong>Github</strong>
+            <i>ekelly14</i>
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/elijah-kelly-32878917a/">
+          <div class="link-container">
+            <strong>LinkedIn</strong>
+            <i>Elijah Kelly</i>
+          </div>
+        </a>
+        <a href="mailto:ekelly14@ymail.com">
+          <div class="link-container">
+            <strong>Email</strong>
+            <i>ekelly14@ymail.com</i>
+            <i>elk8kh@gmail.com</i>
+          </div>
+        </a>
       </div>
     </div>
   </div>
@@ -18,6 +32,11 @@ import TitleBar from '~/components/header/TitleBar.vue'
   export default {
     components: {
       TitleBar
+    },
+    head() {
+      return {
+        title: "Elijah Kelly | Contact"
+      }
     }
   }
 </script>
@@ -33,5 +52,13 @@ import TitleBar from '~/components/header/TitleBar.vue'
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+.link-container {
+  padding: 2rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  background-color:cadetblue;
+  display: flex;
+  flex-direction: column;
 }
 </style>
